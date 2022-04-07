de0cb8b542f6250a8bb5cb88d91f466b7cd058a4
moo_hax <7361922+moohax@users.noreply.github.com>
2022-04-07 16:27:29 -0600

    conftest for pytest. Update mnist model

--- tests/attacks/targets/__init__.py
+++ tests/attacks/targets/__init__.py
@@ -1,13 +1,3 @@
-# import os
-
-# for module in os.listdir(os.path.dirname(__file__)):
-#     if module == "__init__.py" or module[-3:] != ".py":
-#         continue
-#     else:
-#         __import__("targets." + module[:-3], locals(), globals())
-
-# del module
-
 from counterfit.core.targets import CFTarget
 
 from .creditfraud import CreditFraud
