689604bb92385fdd92fa809827e87176b59ce0bc
moo_hax <7361922+moohax@users.noreply.github.com>
2022-04-07 16:25:51 -0600

    Update examples

--- examples/scripting/cf.py
+++ examples/scripting/cf.py
@@ -1,26 +1,19 @@
-# Counterfit is not installed as a package and is not part of search path. 
-
+# imports
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 from counterfit import Counterfit
-from counterfit.core.utils import set_id
-from counterfit.core.optimize import optimize
+from counterfit.core import optimize
 
-# Individual attack
-attack = "hop_skip_jump"
+from targets import CreditFraud
+from targets import DigitKeras
+from targets import Digits
 
-# Set the target
-target = Counterfit.targets()["creditfraud"]
-target = Counterfit.target_builder(target)
+attacks = ["mi_face"]
+target = Digits()
+target.load()
 
-# Individual attack
-cfattack = Counterfit.attack_builder(target, attack)
-results = Counterfit.attack_runner(cfattack)
+for attack in attacks: 
+    cfattack = Counterfit.build_attack(target, attack)
+    Counterfit.run_attack(cfattack)
 
-# Optimized attack
-scan_id = set_id()
-optuna_study = optimize(scan_id, target, attack)
-print(optuna_study.best_trials[0].params)
-
-# Attack with best params
-cfattack = Counterfit.attack_builder(target, attack)
-cfattack.options.update_attack_parameters(optuna_study.best_trials[0].params)
-results = Counterfit.attack_runner(cfattack)
+print(cfattack.results)
